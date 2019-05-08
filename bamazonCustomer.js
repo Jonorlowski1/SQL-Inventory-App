83756f51538e65bde11578e31a42d1dec49cdaff
Jonathan Orlowski <jon.orlowski1@gmail.com>
2019-05-08 07:05:59 -0500

    products displaying through node with chalk npm color added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,27 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
+const chalk = require("chalk");
+const lineBreak = chalk.red("=").repeat(70);
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'bamazon'
+  user: "root",
+  password: "password",
+  database: "bamazon"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
-  // displayProducts();
+  console.log("connected as id " + connection.threadId);
+  displayProducts();
   connection.end();
 });
 
-// function displayProducts() {
-
-// };
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for (let i = 0; i < res.length; i++) {
+      console.log(chalk.blue(res[i].item_id) + ' | ' + chalk.white(res[i].product_name) + ' | ' + chalk.green(res[i].price));
+    }
+    console.log(lineBreak);
+  });
+}
